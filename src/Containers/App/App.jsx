913a242b4a02ccc8a9bb0d5bbb5c62e08eaefb8f
Alexander Alarcon <walarcon14@gmail.com>
2021-02-01 21:40:14 -0500

    Add tailwindcss

--- src/Containers/App/App.jsx
+++ src/Containers/App/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function App() {
-  return <div className="App">Hello world!!</div>;
+  return (
+    <div className="font-bold text-center text-white bg-red-400 App">
+      Hello world!!
+    </div>
+  );
 }
 
 export default App;
